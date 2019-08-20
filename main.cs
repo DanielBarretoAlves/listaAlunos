26f388a88c4596d6b0a34239019870ac480cf22f
Daniel Barreto <danielbarreto559@gmail.com>
2019-08-20 13:37:04 -0300

    calc base

--- main.cs
+++ main.cs
@@ -19,62 +19,24 @@ namespace listaAlunos
             DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(json);
 
             DataTable dataTable = dataSet.Tables["Purtuguex"];
-            //float notaTotal;
+          
             Console.WriteLine(dataTable.Rows.Count);
-            // 2
-            float n1, n2;
-            //foreach (DataRow row in dataTable.Rows)
-            //{
-            //    //Console.WriteLine(row["Nome"] + " - " + row["p1"]);
-            //    var x = row["Nome"];
-            //    Console.WriteLine(x);
-            //    //if (x == "Daniel Barreto Alves")
-            //    //{
-            //    //    Console.WriteLine("Funfou");
-            //    //}
-                
 
-            //}
             for (int i = 0; i < dataTable.Rows.Count; i++)
             {
-                // your index is in i
+
                 var row = dataTable.Rows[i];
                 Console.WriteLine(row["Nome"]);
+                double x = (double)row["p1"];
+                double y = (double)row["p2"];
+                if((x + y) >6.0)
+                {
+                    Console.WriteLine("Passou");
+                }
                
             }
 
-            //dataTable.Columns[0]() =;
 
-            // 0 - item 0
-            // 1 - item 1
-
-
-
-
-
-
-
-            //Account account = JsonConvert.DeserializeObject<Account>(json);
-
-
-
-
-
-
-            //Account account = JsonConvert.DeserializeObject<Account>(json);
-
-            ////Console.WriteLine("Nome: " + account.Nome);
-            ////Console.WriteLine("Nota1: " + account.Nota1);
-            ////Console.WriteLine("Nota2: " + account.Nota2);
-            ////if(account.Nota1 + account.Nota2 > 6)
-            ////{
-            //foreach (object o in account.A1)
-            //{
-            //    Console.WriteLine(o);
-            //}
-            //List<string> names = new List<string>();
-            //}
-            // james@example.com
         }
     }
 }
