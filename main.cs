22d1dd2d551371a505e9fe1cec955e18362b66ba
RodrigoDiJorge <53792442+RodrigoDiJorge@users.noreply.github.com>
2019-08-20 17:26:58 -0300

    Add files via upload

--- main.cs
+++ main.cs
@@ -14,32 +14,27 @@ namespace listaAlunos
     {
         public static void Main(string[] args)
         {
-            string testezao = "listaAlunos.json";
-            var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @testezao);
-            DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(json);
+            Turmas trm = new Turmas();
+            string materia = null;
+            string nome = null;
 
-            DataTable dataTable = dataSet.Tables["Purtuguex"];
-          
-            Console.WriteLine(dataTable.Rows.Count);
-
-            for (int i = 0; i < dataTable.Rows.Count; i++)
-            {
-
-                var row = dataTable.Rows[i];
-                Console.WriteLine(row["Nome"]);
-                double x = (double)row["p1"];
-                double y = (double)row["p2"];
-                if((x + y) >6.0)
-                {
-                    Console.WriteLine("Passou");
-                }
-               
-            }
+            Console.WriteLine("Digite uma materia");
+            materia = Console.ReadLine();
 
 
+            trm.metodo(materia);
+
+            Console.WriteLine("Nome:");
+            materia = null;
+            nome = Console.ReadLine();
+            Console.WriteLine("Materia");
+            materia = Console.ReadLine();
+
+            trm.rendimento(nome, materia);
         }
     }
 }
 
-       
+
+
 
