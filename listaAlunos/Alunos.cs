b7aa74fb308a01c073fc09fcd031314bf89dcc7e
Daniel Barreto <danielbarreto559@gmail.com>
2019-08-26 09:01:15 -0300

    Validador de Nomes

--- listaAlunos/Alunos.cs
+++ listaAlunos/Alunos.cs
@@ -9,12 +9,15 @@ using static System.Console;
 using System.Data;
 
 public class Alunos
-
 {
-    public void fakeMain(string materia, string nome)
+
+
+
+
+    public void fakeMain(string turma, string materia, string nome)
     {
-        string testezao = "@9BM.json";
-        var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + testezao);
+        //string testezao = "listaAlunos.json";
+        var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + turma);
         DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(json);
 
         DataTable dataTable = dataSet.Tables[materia];
@@ -45,9 +48,95 @@ public class Alunos
 
             }
 
+
+
         }
+
     }
+
+    public double getP1(string turma, string materia, string nome)
+    {
+        var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + turma);
+        double p1 = 0.0;
+        DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(json);
+
+        DataTable dataTable = dataSet.Tables[materia];
+        for (int i = 0; i < dataTable.Rows.Count; i++)
+        {
+            var row = dataTable.Rows[i];
+            string checkName = (string)row["Nome"];
+            if (checkName == nome)
+            {
+
+
+
+
+                p1 = (double)row["p1"];
+
+            }
         }
+        return p1;
+    }
+
+    public double getP2(string turma, string materia, string nome)
+    {
+        var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + turma);
+        double p2 = 0.0;
+        DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(json);
+
+        DataTable dataTable = dataSet.Tables[materia];
+        for (int i = 0; i < dataTable.Rows.Count; i++)
+        {
+            var row = dataTable.Rows[i];
+            string checkName = (string)row["Nome"];
+            if (checkName == nome)
+            {
+
+
+
+
+                p2 = (double)row["p2"];
+
+            }
+        }
+        return p2;
+    }
+
+    public bool validadeteName(string turma, string materia, string nome)
+    {
+        bool isVallid = false;
+
+        
+        var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + turma);
+        DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(json);
+
+        DataTable dataTable = dataSet.Tables[materia];
+
+        
+
+        for (int i = 0; i < dataTable.Rows.Count; i++)
+        {
+            var row = dataTable.Rows[i];
+            string checkName = (string)row["Nome"];
+            if (checkName == nome)
+            {
+                isVallid = true;
+
+            }
+            
+         
+
+
+        }
+
+
+
+        //----------------------------------------
+
+        return isVallid;
+    }
+
+}
 
 
 
