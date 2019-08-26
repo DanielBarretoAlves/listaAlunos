037be21b89824b1d5526096821c9b9bc192bfa8b
Daniel Barreto <danielbarreto559@gmail.com>
2019-08-26 09:19:49 -0300

    Remoção do FakeMain da Materia

--- listaAlunos/Materia.cs
+++ listaAlunos/Materia.cs
@@ -15,40 +15,7 @@ namespace listaAlunos
 
 
 
-        public void fakeMain(string turma, string materia, string nome)
-        {
-            
-            var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + turma);
-            DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(json);
-
-            DataTable dataTable = dataSet.Tables[materia];
-            for (int i = 0; i < dataTable.Rows.Count; i++)
-            {
-                var row = dataTable.Rows[i];
-                string checkName = (string)row["Nome"];
-                if (checkName == nome)
-                {
-                    Console.WriteLine(row["Nome"]);
-                    double p1 = (double)row["p1"];
-                    double p2 = (double)row["p2"];
-                    double media = (p1 + p2) / 2;
-                    Console.WriteLine("A media do(a) aluno(a) " + row["Nome"] + ": " + media);
-                    if (media >= 6.0)
-                    {
-                        Console.WriteLine("Aprovado(a)");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Reprovou");
-                    }
-
-                }
-
-
-
-            }
-
-        }
+     
 
 //--------------------------------------------------------------------------------
 
