4b820d44ec7000d56e2c6d542b104d10465c315c
Daniel Barreto <danielbarreto559@gmail.com>
2019-08-26 09:19:00 -0300

    Remover FakeMain

--- listaAlunos/Alunos.cs
+++ listaAlunos/Alunos.cs
@@ -14,45 +14,7 @@ public class Alunos
 
 
 
-    public void fakeMain(string turma, string materia, string nome)
-    {
-        //string testezao = "listaAlunos.json";
-        var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + turma);
-        DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(json);
-
-        DataTable dataTable = dataSet.Tables[materia];
-
-        //Console.WriteLine(dataTable.Rows.Count);
-
-        for (int i = 0; i < dataTable.Rows.Count; i++)
-        {
-            var row = dataTable.Rows[i];
-            string checkName = (string)row["Nome"];
-            if (checkName == nome)
-            {
-
-
-                Console.WriteLine(row["Nome"]);
-                double p1 = (double)row["p1"];
-                double p2 = (double)row["p2"];
-                double media = (p1 + p2) / 2;
-                Console.WriteLine("A media do(a) aluno(a) " + row["Nome"] + ": " + media);
-                if (media >= 6.0)
-                {
-                    Console.WriteLine("Aprovado(a)");
-                }
-                else
-                {
-                    Console.WriteLine("Reprovou");
-                }
-
-            }
-
-
-
-        }
-
-    }
+   
 
     public double getP1(string turma, string materia, string nome)
     {
