0ac3fed5e9ebbe2fd56a057fc1008f5cb1929aef
Daniel Barreto <danielbarreto559@gmail.com>
2019-08-30 10:58:48 -0300

    Teste De Classes

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace classManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Materia topic = new Materia();
+            Turma classroom = new Turma();
+            Console.WriteLine(topic.GetAluno("9BM2.json", classroom.selectMateria("9BM2.json")));
+
+        }
+    }
+}
